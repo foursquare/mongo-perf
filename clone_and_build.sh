b4db87713796c0bcbc5592b2e5571da6b6c19edd
Neil Sanchala <nsanch@foursquare.com>
2012-06-13 15:46:22 -0400

    better cloning script

--- clone_and_build.sh
+++ clone_and_build.sh
@@ -5,12 +5,24 @@ set -x
 
 mkdir -p perfrunner
 cd perfrunner
-git clone git@github.com:foursquare/mongo.git
-cd mongo
-git checkout r2.0.5-fs
+if [ -d mongo ]; then
+  cd mongo
+  git checkout HEAD shell/mongo_vstudio.cpp
+  git checkout r2.0.5-fs
+  git pull
+else
+  git clone git@github.com:foursquare/mongo.git
+  cd mongo
+  git checkout r2.0.5-fs
+fi
 scons --full all
 scons --full mongoclient
 cd ..
-git clone git@github.com:foursquare/mongo-perf.git
+
+
+if ! [ -d mongo-perf ]; then
+  git clone git@github.com:foursquare/mongo.git
+fi
 cd mongo-perf
+git pull
 scons
